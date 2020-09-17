01d4cefdfa8d55a88720544e167152b1cf8f67cd
Ann Peng <boaimpact@gmail.com>
2020-09-17 23:22:59 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,5 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
+
+mongoose.connect('mongodb://localhost/expense-tracker', { useNewUrlParser: true, useUnifiedTopology: true })
+
+const db = mongoose.connection
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connect!')
+})
+
 
 app.get('/', (req, res) => {
   res.send('Hello World')
